506085981e45cc8f32064f5f5d8b028d71b07f4d
David Albert <davidbalbert@gmail.com>
2012-04-13 19:50:16 -0400

    removed extra prints

--- tasks.py
+++ tasks.py
@@ -1,7 +1,5 @@
 from constants import *
 
 def clone_and_push():
-    print "cloning %s to %s" % (HG_REPO, REPO_PATH)
     os.system("hg clone %s %s" % (HG_REPO, REPO_PATH))
-    print "removing %s" % REPO_PATH
     os.system("rm -rf %s" % REPO_PATH)
