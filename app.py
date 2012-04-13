4638cda2fcdf3126408ce86c877a5f1e15d7472f
David Albert <davidbalbert@gmail.com>
2012-04-13 16:52:49 -0400

    successfully clone hg repo into tmp

--- app.py
+++ app.py
@@ -8,11 +8,19 @@ VERSION = "0.0.1"
 HG_REPO = os.environ.get('HG_REPO')
 GIT_REPO = os.environ.get('GIT_REPO')
 
+REPO_NAME = HG_REPO.split("/")[-1]
+
 app.debug = os.environ.get('DEBUG', False)
 
 @app.route('/')
 def index():
     return 'hg-git mirror v%s<br><br>\nmirroring %s<br>\nto %s' % (VERSION, HG_REPO, GIT_REPO)
+
+@app.route('/hook', methods=["POST"])
+def hook():
+    print "cloning %s to ./tmp/%s" % (HG_REPO, REPO_NAME)
+    os.system("hg clone %s tmp/%s" % (HG_REPO, REPO_NAME))
+    return "mirroring\n"
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
