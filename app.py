af6022707cea50c27301b2e7143d9346c802e4a6
David Albert <davidbalbert@gmail.com>
2012-04-13 16:24:46 -0400

    print status message on /

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+VERSION = "0.0.1"
+
+HG_REPO = os.environ.get('HG_REPO')
+GIT_REPO = os.environ.get('GIT_REPO')
+
+app.debug = os.environ.get('DEBUG', False)
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return 'hg-git mirror v%s<br><br>\nmirroring %s<br>\nto %s' % (VERSION, HG_REPO, GIT_REPO)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
