38d0a439c989f974f5c2c95466fbd1482233fca9
David Albert <davidbalbert@gmail.com>
2012-04-13 21:32:02 -0400

    work with normal git remote urls

--- constants.py
+++ constants.py
@@ -5,6 +5,10 @@ VERSION = "0.0.1"
 HG_REPO = os.environ.get('HG_REPO')
 GIT_REPO = os.environ.get('GIT_REPO')
 
+if not GIT_REPO.startswith("git+ssh://"):
+    # we have a standard format git remote
+    GIT_REPO = "git+ssh://" + "/".join(GIT_REPO.split(":"))
+
 REPO_NAME = HG_REPO.split("/")[-1]
 REPO_PATH = "tmp/%s" % REPO_NAME
 
